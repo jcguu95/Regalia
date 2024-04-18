e383b225d6abce28140218ca7f7f8c9e748cca2f
jcguu95 <jcguu95@gmail.com>
2024-04-18 07:49:28 -0400

    Progressed on VECTOR-PUSH-EXTEND

--- Code/function.vector-push-extend.lisp
+++ Code/function.vector-push-extend.lisp
@@ -1,1 +1,22 @@
+;; TODO Under Construction
+
+;;;; Function VECTOR-PUSH-EXTEND
+;;;; https://www.lispworks.com/documentation/HyperSpec/Body/f_vec_ps.htm
+
+(cl:in-package #:regalia)
+
+(defgeneric vector-push-extend (new-element vector &optional extension))
+
 ;; TODO Reuse code of VECTOR-PUSH and ADJUST-ARRAY.
+(defmethod vector-push-extend (new-element (vector vector) &optional extension)
+  ;; (when (< (fill-pointer vector)
+  ;;          (array-dimension vector 0))
+  ;;   (let ((index (fill-pointer vector)))
+  ;;     (incf (fill-pointer vector))
+  ;;     (setf (aref index vector) new-element)
+  ;;     index))
+  )
+
+(defmethod vector-push-extend (new-element vector &optional extension)
+  (error 'type-error :datum array :expected-type 'array))
+
